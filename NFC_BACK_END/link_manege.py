41ed58c9206a612ce5101e1a89720438440e44cb
EzatMatin <ezatwakily1377@gmail.com>
2023-04-13 18:29:14 -0300

    file reader and put into Link url is ready

--- NFC_BACK_END/link_manege.py
+++ NFC_BACK_END/link_manege.py
@@ -1,28 +1,26 @@
-from flask import Flask, jsonify, request,make_response
+from flask import Flask, jsonify, request, make_response
 import webbrowser
 from database import *
 
 app = Flask(__name__)
-@app.route('/gerenciador',methods=['POST'])
+
+
+@app.route('/gerenciador', methods=['POST'])
 def entrada():
     create_table('pagteste1')
     links = request.json
-    for linha in range(len(links["links"] )):
+    for linha in range(len(links["links"])):
         link = links["links"][linha]
-        inserir_links('pagteste1',link)
-    return links    
-@app.route('/consultor',methods=['GET'])
-def link(): 
+        inserir_links('pagteste1', link)
+    return links
+
+
+@app.route('/consultor', methods=['GET'])
+def link():
     numero_acess = update('pagteste1')
     #link = webbrowser.open(numero_acess[2],new=0)
     print(numero_acess)
-    return make_response(jsonify(numero_acess,'guia aberta com sucesso'))
+    return make_response(jsonify(numero_acess, 'guia aberta com sucesso'))
 
 
-
-
-
-app.run(port=5000,host='localhost',debug=True) 
-
-
-
+app.run(port=5000, host='localhost', debug=True)
--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ dragText = dropArea.querySelector("header");
 button = dropArea.querySelector("button");
 input = dropArea.querySelector("input");
 
+
 let file;
 
 input.addEventListener("change", function () {
@@ -30,30 +31,40 @@ dropArea.addEventListener("drop", (event) => {
   showFile();
 });
 
-function showFile() {
+async function showFile() {
   let fileType = file.type;
   let validExtentions = ["text/plain"];
+  var url;
   if (validExtentions.includes(fileType)) {
-    let fileReader = new fileReader();
-    fileReader.onload = () => {
-      let fileURL = fileReader.result;
-      fetch.up;
-    };
-    fileReader.readAsDataURL(File);
+    let fileReader = new FileReader();
+    fileReader.onload = function () {
+      url = fileReader.result;
+      uploadFile(url);
+    }
+    fileReader.readAsDataURL(file);
   } else {
     alert("Please Select a Txt");
   }
 }
-   // transfer files in this order using Json
-{   
-  "links" :[
-  "https://www.python.org/1",
-  "https://www.python.org/2",
-  "https://www.python.org/3",
-  "https://www.python.org/1",
-  "https://www.python.org/2",
-  "https://www.python.org/3", 
-  "https://www.python.org/4", 
-  "https://www.python.org/4",
-  "https://www.python.org/5"
-  ]}
+
+async function uploadFile(url) {
+  const linksArray = [url];
+  const linksMap = {
+    'links': linksArray,
+  };
+  // starting the upload
+  // for localhost you can use: http://localhost:5000 after semicolon is port number. 
+  let response = await fetch('https:link-to-flask-application', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(linksMap)
+  });
+
+
+  // upload finished
+  if (response.status === 200 || response.status === 201) {
+    alert("Uploaded successfully");
+  } else {
+    alert("Failed to upload");
+  }
+}
