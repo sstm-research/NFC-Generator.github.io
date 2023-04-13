bd15415102e31610a8477e9961c6125157cdf7f4
EzatMatin <ezatwakily1377@gmail.com>
2023-04-13 09:15:38 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,44 +5,55 @@ input = dropArea.querySelector("input");
 
 let file;
 
-
-
-input.addEventListener("change", function(){
+input.addEventListener("change", function () {
   file = this.files[0];
   showFile();
 });
 
 //user drag file over area//
-dropArea.addEventListener("dragover", (event)=>{
-  event.preventDefault(); 
+dropArea.addEventListener("dragover", (event) => {
+  event.preventDefault();
   dropArea.classList.add("active");
   dragText.textContent = "Release to Upload";
 });
 
 //user leave area without drop the file//
-dropArea.addEventListener("dragleave", ()=>{
+dropArea.addEventListener("dragleave", () => {
   dropArea.classList.remove("active");
   dragText.textContent = "Drag your file with poap links here";
 });
- 
+
 //user drop the file on area//
-dropArea.addEventListener("drop", (event)=>{
-  event.preventDefault(); 
+dropArea.addEventListener("drop", (event) => {
+  event.preventDefault();
   file = event.dataTransfer.files[0];
   showFile();
 });
 
-function showFile(){
+function showFile() {
   let fileType = file.type;
-  let validExtentions = ["text/plain"]
-    if(validExtentions.includes(fileType)){
-      let fileReader = new fileReader();
-      fileReader.onload = () =>{
-        let fileURL = fileReader.result; 
-        fetch.up
-      }
-      fileReader.readAsDataURL(file);
-    }else{
-      alert("Please Select a Txt") 
-    }
+  let validExtentions = ["text/plain"];
+  if (validExtentions.includes(fileType)) {
+    let fileReader = new fileReader();
+    fileReader.onload = () => {
+      let fileURL = fileReader.result;
+      fetch.up;
+    };
+    fileReader.readAsDataURL(File);
+  } else {
+    alert("Please Select a Txt");
+  }
 }
+   // transfer files in this order using Json
+{   
+  "links" :[
+  "https://www.python.org/1",
+  "https://www.python.org/2",
+  "https://www.python.org/3",
+  "https://www.python.org/1",
+  "https://www.python.org/2",
+  "https://www.python.org/3", 
+  "https://www.python.org/4", 
+  "https://www.python.org/4",
+  "https://www.python.org/5"
+  ]}
