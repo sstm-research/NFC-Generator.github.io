59bc5ddc89dbe7a8df32f48179f97de6879b354b
DanielCodeSher <120428531+DanielCodeSher@users.noreply.github.com>
2023-04-13 08:43:11 -0300

    CÓDIGO NFC

---
+++ NFC_BACK_END/database.py
@@ -0,0 +1,50 @@
+import mysql.connector
+conn = mysql.connector.connect(
+    host ='localhost',
+    database ='GERENCIADOR',
+    user ='root',
+    password ='24042022'
+    )
+
+def create_table(pag):
+    cursor = conn.cursor()
+    sql = (f'CREATE TABLE {pag}(ID smallint primary key auto_increment, links longtext,STATUS text);')
+    cursor.execute(sql)   
+    conn.commit()
+    cursor.close
+    
+def inserir_links(pag,link):
+    cursor = conn.cursor()
+    sql = (f'INSERT INTO {pag}(links,STATUS) VALUE("{link}","not acess");')
+    cursor.execute(sql)
+    conn.commit()
+    
+
+def leitura(pag):
+    cursor = conn.cursor() 
+    sql  = (f"SELECT * FROM {pag};")
+    cursor.execute(sql)
+    result = cursor.fetchmany(size=200);
+    cursor.close
+    return result
+
+def update(tabela):
+    dados =leitura(tabela)
+    cursor = conn.cursor()
+    tamanho = len(dados)
+    for linha in range(tamanho):
+        acesso = dados[linha][2]
+        link = dados[linha][1]
+        id = dados[linha][0]
+        if acesso == 'not acess':
+            sql = (f"update {tabela} set status = 'acessado' where ID  = {id};")
+            cursor.execute(sql)
+            conn.commit()
+            cursor.close()        
+            return id,tamanho,link 
+            break       
+        elif id == tamanho:
+            return 'Todos os Links Foram acessados.'
+    
+    
+    
---
+++ NFC_BACK_END/link_manege.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request,make_response
+import webbrowser
+from database import *
+
+app = Flask(__name__)
+@app.route('/gerenciador',methods=['POST'])
+def entrada():
+    create_table('pagteste1')
+    links = request.json
+    for linha in range(len(links["links"] )):
+        link = links["links"][linha]
+        inserir_links('pagteste1',link)
+    return links    
+@app.route('/consultor',methods=['GET'])
+def link(): 
+    numero_acess = update('pagteste1')
+    #link = webbrowser.open(numero_acess[2],new=0)
+    print(numero_acess)
+    return make_response(jsonify(numero_acess,'guia aberta com sucesso'))
+
+
+
+
+
+app.run(port=5000,host='localhost',debug=True) 
+
+
+
